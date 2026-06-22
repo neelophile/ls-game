d03e0e74c5a6019d957dfc3851a439c6a9e273c2
neelophile <alwaysgaming1610@gmail.com>
2026-06-22 12:26:24 +0530

    Fixed Alembic not being able to read URI

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,12 @@
 from logging.config import fileConfig
-
+from os import getenv
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from dotenv import load_dotenv
 from alembic import context
 
+
+load_dotenv()
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -13,6 +15,8 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+config.set_main_option("sqlalchemy.url", getenv("URI"))
 
 # add your model's MetaData object here
 # for 'autogenerate' support
